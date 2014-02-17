cc4f0e52699ac8297d2d937892aded105cbe312d
Aron Aperauch <aperauch@gmail.com>
2014-02-17 12:08:17 -0500

    Test

--- src/taskmgt/Serializer.java
+++ src/taskmgt/Serializer.java
@@ -15,6 +15,8 @@ import java.util.HashSet;
 import java.util.logging.Level;
 import java.util.logging.Logger;
  
+//Good job alex!
+
 public class Serializer<Type> {
     private final String fileName;
     private final String dirPath;
