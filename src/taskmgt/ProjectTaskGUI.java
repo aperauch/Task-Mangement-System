ba2b99b13aef6d34b3be316ca46025421b112fb1
Rui <ruiyangwind@hotmail.com>
2014-02-19 13:34:45 -0500

    some Tweak

--- src/taskmgt/ProjectTaskGUI.java
+++ src/taskmgt/ProjectTaskGUI.java
@@ -315,7 +315,7 @@ public final class ProjectTaskGUI extends javax.swing.JFrame {//implements ListS
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(39, 39, 39)
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(199, Short.MAX_VALUE))
+                .addContainerGap(28, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
